36cda2ef4670bb83a325a6d6d9e618a46751b98b
zsuz <avdosevstas@mail.ru>
2019-07-13 15:12:07 +0400

    +good ajax

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -12,58 +12,66 @@
 // goAjaxRequest({type:'POST',data:data,url:'/api/v1/example-upload-file'});
 
 
-function goAjaxRequest(obj) {
+function goAjaxRequest(obj,fileLoad) {
     if (!obj.type)
         obj.type = 'GET';
     //if (!obj.dataType)
       //  obj.dataType = 'json';//html
-    $.ajax(
-        {
-            type: obj.type,
-            data: obj.data,
-            url: obj.url,
-            //processData: false, // Не обрабатываем файлы
-            //contentType: false, // Так jQuery скажет серверу что это строковой запрос
-            success: function (xhr, status, jqXHR) {
-                //if(jqXHR.status==200){//EXAMPLE STATUS
-                    //DO SOMETHING
-                //}
-                if (obj.funcSuccess) {
-                    try {
-                        obj.funcSuccess(xhr, status, jqXHR);
-                    }
-                    catch (e) {
-                        console.log('Ошибка ' + e.name + ":" + e.message + "\n" + e.stack);
-                    }
 
+    let ajaxObj={
+        type: obj.type,
+        data: obj.data,
+        url: obj.url,
+        //processData: false, // Не обрабатываем файлы
+        //contentType: false, // Так jQuery скажет серверу что это строковой запрос
+        success: function (xhr, status, jqXHR) {
+            //if(jqXHR.status==200){//EXAMPLE STATUS
+            //DO SOMETHING
+            //}
+            if (obj.funcSuccess) {
+                try {
+                    obj.funcSuccess(xhr, status, jqXHR);
                 }
-            },
-            error: function (xhr, status, error) {
-                alert("ошибка загрузки");
-                if (obj.funcError)
-                    obj.funcError(xhr, status, error);
-            },
-            // shows the loader element before sending.
-            beforeSend: function () {
-                if (obj.funcBeforeSend)
-                    obj.funcBeforeSend();
-                //  PreloaderShowChange(true);
-            },
-            // hides the loader after completion of request, whether successfull or failor.
-            complete: function (xhr, status, jqXHR) {
-                if (obj.funcComplete) {
-                    try {
-                        obj.funcComplete(xhr, status, jqXHR);
-                    }
-                    catch (e) {
-                        console.log('Ошибка ' + e.name + ":" + e.message + "\n" + e.stack);
-                    }
+                catch (e) {
+                    console.log('Ошибка ' + e.name + ":" + e.message + "\n" + e.stack);
                 }
 
-                //PreloaderShowChange(false);
-            },
-            dataType: obj.dataType//'html'
-        });
+            }
+        },
+        error: function (xhr, status, error) {
+            alert("ошибка загрузки");
+            if (obj.funcError)
+                obj.funcError(xhr, status, error);
+        },
+        // shows the loader element before sending.
+        beforeSend: function () {
+            if (obj.funcBeforeSend)
+                obj.funcBeforeSend();
+            //  PreloaderShowChange(true);
+        },
+        // hides the loader after completion of request, whether successfull or failor.
+        complete: function (xhr, status, jqXHR) {
+            if (obj.funcComplete) {
+                try {
+                    obj.funcComplete(xhr, status, jqXHR);
+                }
+                catch (e) {
+                    console.log('Ошибка ' + e.name + ":" + e.message + "\n" + e.stack);
+                }
+            }
+
+            //PreloaderShowChange(false);
+        },
+        dataType: obj.dataType//'html'
+    };
+    if(fileLoad){
+        //processData: false, // Не обрабатываем файлы
+        //contentType: false,
+        ajaxObj.processData=false;
+        ajaxObj.contentType=false;
+    }
+
+    $.ajax(ajaxObj);
 }
 
 ;;;
