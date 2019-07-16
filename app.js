1d954915cfd3a7fad85c45d5a336450e024de393
Vladislav001 <VladGurjev@mail.ru>
2019-07-16 20:13:00 +0400

    Добавил 2 подключение к БД (MongoAtlas)

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const app = express();
 
 // connect to DB
 mongoose.connect('mongodb://admin:123456v@ds155606.mlab.com:55606/repair-assistant', {useNewUrlParser: true});
+//mongoose.connect('mongodb+srv://admin:123456v@cluster0-qjjoz.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true});
+
 mongoose.set('useFindAndModify', false);
 
 
