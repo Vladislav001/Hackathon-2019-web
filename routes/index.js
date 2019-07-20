1f63e07a92ad44c793ef47ba57157ca516fe2054
woodanok <52826264+woodanok@users.noreply.github.com>
2019-07-20 16:18:28 +0400

    Верстка детальной

--- routes/index.js
+++ routes/index.js
@@ -28,14 +28,14 @@ const upload = multer({
     //fileFilter: fileFilter
 });
 
-
+router.get('/detail-project', require('./detail_project/cards').get);
 router.get('/', require('./example/main_example').get);
 
 router.post('/add-example', require('./example/add_example').post);
 router.post('/delete-example/id:_id', require('./example/delete_example').post);
 router.post('/update-example/id:_id', require('./example/update_example').post);
 router.get('/example/id:_id', require('./example/detail_example').get);
-
+router.get('/', require('./example/main_example').get);
 
 ////**** API ****\\\\
 
