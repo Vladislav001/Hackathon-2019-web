d1b9d272d0783b448af928555de2153426b391e0
woodanok <52826264+woodanok@users.noreply.github.com>
2019-07-21 00:05:13 +0400

    Затянул изменения с develop

--- routes/detail_project/detail_project.js
+++ routes/detail_project/detail_project.js
@@ -5,7 +5,7 @@ const Technology = require('../../models/technology');
 exports.get = async function (req, res) {
     try {
         let students = await Students.find({});
-        let project = await Project.find({});
+        let project = await Project.findOne({_id: '5d33667af2e60a2d88be9c9e'});
         let technology = await Technology.find({});
         console.log(project);
         // console.log(students);
