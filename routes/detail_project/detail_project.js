ca2264f13c1e27f23c33fe3020cd84a75fd99455
woodanok <52826264+woodanok@users.noreply.github.com>
2019-07-21 01:54:25 +0400

    сохранил изменения Вадим

--- routes/detail_project/detail_project.js
+++ routes/detail_project/detail_project.js
@@ -1,12 +1,14 @@
 const Students = require('../../models/student');
 const Project = require('../../models/project');
 const Technology = require('../../models/technology');
+const Employer = require('../../models/employer');
 
 exports.get = async function (req, res) {
     try {
         let students = await Students.find({});
         let project = await Project.findOne({_id: '5d33667af2e60a2d88be9c9e'});
-        let technology = await Technology.find({});
+        let employer = await Employer.findOne({_id:project.employerId});
+        let technology = await Technology.find({projectId:project._id});
         console.log(project);
         // console.log(students);
         // projects.forEach(example => {
@@ -16,6 +18,9 @@ exports.get = async function (req, res) {
 
         res.render('detail_project/detail_project', {
                 students:students,
+                technology:technology,
+                project:project,
+                employer:employer,
         });
 
     } catch (err) {
