71096f6cad8cff13d3fd1745a7604799ac681026
woodanok <52826264+woodanok@users.noreply.github.com>
2019-07-20 23:53:10 +0400

    Затянул изменения с develop

--- routes/detail_project/detail_project.js
+++ routes/detail_project/detail_project.js
@@ -1,5 +1,25 @@
-exports.get = async function (req, res) {
+const Students = require('../../models/student');
+const Project = require('../../models/project');
+const Technology = require('../../models/technology');
 
-    res.render('detail_project/detail_project');
+exports.get = async function (req, res) {
+    try {
+        let students = await Students.find({});
+        let project = await Project.find({});
+        let technology = await Technology.find({});
+        console.log(project);
+        // console.log(students);
+        // projects.forEach(example => {
+        //     example.file = `${req.headers['host']}/${example.file}`;
+        //     data.push(example);
+        // });
+
+        res.render('detail_project/detail_project', {
+                students:students,
+        });
+
+    } catch (err) {
+        throw err;
+    }
 
 };
